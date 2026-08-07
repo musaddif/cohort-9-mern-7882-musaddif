18933f687ade3abf6d63dd38fafb72b8353df2cf
musaddif <musaddifktk@gmail.com>
2026-08-07 08:22:50 -0700

    create react js project with vite

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,8 @@
+
+function App() {
+    return (
+    <>
+    <h1>notes web</h1>
+    </>)
+}
+export default App;
---
+++ Frontend/vite.config.js
@@ -0,0 +1,28 @@
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+// })
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+      },
+    },
+  },
+});
